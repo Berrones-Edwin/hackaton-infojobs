4052e75b69b774c268b151e9ede3ed596885a2a6
Berrones Edwin <Berrones.Edwin@hotmail.com>
2023-05-27 12:30:03 -0600

    feat component to view the google form

---
+++ src/app/components/Review.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Stack, } from '@chakra-ui/react'
+
+export default function Review() {
+    return (
+        <Stack minHeight={'100vh'} display={'flex'} justifyContent={'center'} alignItems={'center'} >
+            <Stack>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeJiXgFE63VJ5F_pjNWq8ZEs2vBfIuUvxMa7Dh6TlPpTgNMbQ/viewform?embedded=true" width="640" height="2313" frameBorder="0" marginHeight={0} marginWidth={0}>Loading...</iframe>
+            </Stack>
+        </Stack>
+    )
+}
