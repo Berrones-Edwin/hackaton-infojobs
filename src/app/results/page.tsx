4c6627cf64e6b427e9d5339b8f0cb35ac1200e62
Berrones Edwin <Berrones.Edwin@hotmail.com>
2023-05-30 09:50:56 -0600

    fix change fetch api experimental to axios

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,14 +1,18 @@
 import papa from 'papaparse'
 import Form from '../components/Form';
+import axios from 'axios'
 
 async function getData() {
-    const response: any = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vSwvE7A-cuonAVbUBjACrD1lgWrFBJs2j3ZT4RyDpnETxQkkzUmya9ZUF7a9gY6uRlenqg7za5CJGQ4/pub?output=csv', {
+
+    const response: any = await axios({
+        url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwvE7A-cuonAVbUBjACrD1lgWrFBJs2j3ZT4RyDpnETxQkkzUmya9ZUF7a9gY6uRlenqg7za5CJGQ4/pub?gid=1229312000&single=true&output=csv',
         headers: {
+
             "Content-Type": "application/blob",
         },
-    }).then((res) => res.text())
-
-
+        responseType:'blob'
+    }).then(res=>res.data)
+    
     let dataJson = null
     await papa.parse(response, {
         header: true,
